4d91e33ccb0d80e519266b0b5e73092cb16a3605
Sebastian Rueda <jrueda03@gmail.com>
2022-01-08 13:24:27 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CatCards
+
+Developed with Unreal Engine 4
---
+++ Source/CatCards/CatCardsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CatCardsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CATCARDS_API ACatCardsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
